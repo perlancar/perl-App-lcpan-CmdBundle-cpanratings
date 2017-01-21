01032f40581a08128f94173625a3bdddcbcc2352
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-01-21 21:59:21 +0700

    Initial commit

---
+++ lib/App/lcpan/CmdBundle/cpanratings.pm
@@ -0,0 +1,19 @@
+package App::lcpan::CmdBundle::cpanratings;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: More lcpan subcommands related to CPAN Ratings
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<lcpan>
+
+L<http://cpanratings.perl.org>
