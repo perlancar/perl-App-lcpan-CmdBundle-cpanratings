942ebdc0431301c550631f70f58469f0c046d301
perlancar (@pc-office) <perlancar@gmail.com>
2017-01-24 18:45:13 +0700

    Add subcommand: cpanratings-get-reviews

---
+++ lib/App/lcpan/Cmd/cpanratings_get_reviews.pm
@@ -0,0 +1,55 @@
+package App::lcpan::Cmd::cpanratings_get_reviews;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::Any::IfLOG '$log';
+
+require App::lcpan;
+
+our %SPEC;
+
+$SPEC{handle_cmd} = {
+    v => 1.1,
+    summary => 'Get CPAN Ratings reviews for a dist/module',
+    args => {
+        %App::lcpan::common_args,
+        %App::lcpan::mod_or_dist_args,
+    },
+};
+sub handle_cmd {
+    my %args = @_;
+
+    my $state = App::lcpan::_init(\%args, 'ro');
+    my $dbh = $state->{dbh};
+
+    my ($dist, $file_id, $cpanid, $version);
+    {
+        # first find dist
+        if (($file_id, $cpanid, $version) = $dbh->selectrow_array(
+            "SELECT file_id, cpanid, version FROM dist WHERE name=? AND is_latest", {}, $args{module_or_dist})) {
+            $dist = $args{module_or_dist};
+            last;
+        }
+        # try mod
+        if (($file_id, $dist, $cpanid, $version) = $dbh->selectrow_array("SELECT m.file_id, d.name, d.cpanid, d.version FROM module m JOIN dist d ON m.file_id=d.file_id WHERE m.name=?", {}, $args{module_or_dist})) {
+            last;
+        }
+    }
+    $file_id or return [404, "No such module/dist '$args{module_or_dist}'"];
+
+    require WWW::CPANRatings;
+    my @reviews = WWW::CPANRatings->new->get_reviews($dist);
+    for (@reviews) {
+        delete $_->{dist_link};
+        delete $_->{user_link};
+        delete $_->{header};
+    }
+    [200, "OK", \@reviews, {'table.fields'=>[]}];
+}
+
+1;
+# ABSTRACT:
